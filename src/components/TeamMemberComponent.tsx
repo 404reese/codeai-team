c8c3370317f82c38ac96c3b458e9cfe5ddd288fe
Riddhesh <135740066+404reese@users.noreply.github.com>
2025-02-09 15:48:03 +0530

    frame-padding-reduced

--- src/components/TeamMemberComponent.tsx
+++ src/components/TeamMemberComponent.tsx
@@ -13,12 +13,12 @@ interface TeamMember {
   
     return (
       <div
-        className="team-member bg-fuchsia-950 p-4 rounded-xl overflow-hidden w-40 h-96 flex items-center justify-center flex-col text-center hover:w-72 hover:h-96 hover:scale-110 hover:bg-violet-950 transition-all duration-400 transform origin-center"      
+        className="team-member bg-fuchsia-950 pt-16 rounded-xl overflow-hidden w-40 h-96 flex items-center justify-center flex-col text-center hover:w-72 hover:h-96 hover:scale-110 hover:bg-violet-950 transition-all duration-400 transform origin-center"      
         onMouseEnter={() => setIsHovered(true)}
         onMouseLeave={() => setIsHovered(false)}
       >
 
-         <div className={`w-full text-white text-center p-2  text-white rounded-t-xl ${isHovered ? 'opacity-100' : 'opacity-0'} transition-all duration-400`}>
+         <div className={`w-full text-white text-center text-white rounded-t-xl ${isHovered ? 'opacity-100' : 'opacity-0'} transition-all duration-400`}>
           <p className="text-lg font-bold">{position}</p>
           <h3 className="text-sm text-fuchsia-400 font-bold">{name}</h3>
         </div>
